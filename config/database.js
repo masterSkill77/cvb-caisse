79bcbf14419be44702693fb1d8783662a9866f10
mahitsilazafabrila@gmail.com <mahitsilazafabrila@gmail.com>
2021-08-18 13:31:22 +0200

    make it better

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports={
   username:process.env.DB_USERNAME || "cvbshopmg_cvbshopmg",
-  password:process.env.DB_PASSWORD || "analamitsy12345!",
+  password:process.env.DB_PASSWORD || "123456789analamahitsy!",
   database:process.env.DB_DATABASE || "cvbshopmg_analamahitsy",
   host:process.env.DB_HOST || "144.91.112.39",
   dialect: process.env.DB_DIALECT|| "mysql",
