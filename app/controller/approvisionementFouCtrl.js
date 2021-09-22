a87f1e1aef90d3f51fa24e2e4645b9a2031a2e5d
mahitsilazafabrila@gmail.com <mahitsilazafabrila@gmail.com>
2021-09-22 20:58:47 +0200

    sound

--- app/controller/approvisionementFouCtrl.js
+++ app/controller/approvisionementFouCtrl.js
@@ -212,9 +212,9 @@ const addapprovisionnfinal =async(req,res)=>{
         }
         const Payementfous = await Payementfou.create(data);
        
+        // approfoulist = [];
  
         return res.json(Payementfous)
-        approfoulist = [];
         
 
 }
