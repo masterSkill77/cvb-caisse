84e3267f1d6c811a7df3506979c191d4d42c5fed
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2022-01-26 17:51:29 +0300

    fix commande datatable

--- public/projet/commande/historique.js
+++ public/projet/commande/historique.js
@@ -1,16 +1,74 @@
 new Vue({
     el:"#histo",
     data:{
+        headers: [{
+            text: 'id',
+            align: 'start',
+            filterable: false,
+            value: 'id',
+        },
+        {
+            text: 'Client',
+            value: 'cli'
+        },
+        {
+            text: 'Catégorie',
+            value: 'category'
+        },
+        {
+            text: 'Produit',
+            value: 'name'
+        },
+        {
+            text: 'Unité',
+            value: 'unite'
+        },
+        {
+            text: 'Quantité',
+            value: 'qt'
+        },
+        {
+            text: 'Conditionnement',
+            value: 'conditionnement'
+        },
+        {
+            text: 'Prix',
+            value: 'prixVente'
+        },
+        {
+            text: 'Mode',
+            value: 'mode'
+        },
+        {
+            text: 'Date',
+            value: 'datecom'
+        },
+        {
+            text: 'Sous-total',
+            value: 'sousTotal'
+        },
+        {
+            text: 'Action',
+            value: 'action'
+        },
+    ],
         website:'/affichagecommande/',
-        liste:{},
-        clients:{},
+        liste:[],
+        clients:[],
+        search : ''
     },
     methods:{
 
     },
     mounted(){
-        axios.get(this.website).then(response=>{this.liste=response.data})
-        axios.get(this.website).then(({data})=>{this.liste=data.date})
-        axios.get(this.website).then(response=>console.log(response.data))
-    }
+        axios.get(this.website).then(response=>{
+            this.liste=response.data.date;
+            console.log(this.liste);
+            this.liste.map((commande) => {
+                commande.conditionnement = (commande.condition == 1) ? 'Gros' : 'Détail';
+                commande.sousTotal = (commande.condition == 0) ? (commande.pudetail * commande.qt) : (commande.pugros * commande.qt)
+            })
+        })
+    },
+    vuetify : new Vuetify()
 })
