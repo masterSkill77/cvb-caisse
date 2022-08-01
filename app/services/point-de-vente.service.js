aeee69b47e053553c1dd46cf138a42562557a4c3
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2022-08-01 13:46:47 +0300

    fix model name variable in store

--- app/services/point-de-vente.service.js
+++ app/services/point-de-vente.service.js
@@ -1,10 +1,15 @@
-const { PointDeVente } = require("./../models/index");
+const { point_de_vente } = require("./../models/index");
 const create_store = async function (name, contact, adresse) {
   try {
     const data = { name, contact, adresse };
-    return await PointDeVente.create(data);
+    return await point_de_vente.create(data);
   } catch (error) {
     throw new Error(e.message);
   }
 };
-module.exports = { create_store };
+
+const get_all = async function () {
+  return await point_de_vente.findAll();
+};
+
+module.exports = { create_store, get_all };
